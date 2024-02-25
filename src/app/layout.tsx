f60e56b59cb27c2edba05854e748f98d60734523
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-02-25 15:23:59 -0300

    Add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,37 @@ export default function RootLayout({
       lang="pt-BR"
     >
       <head>
+        {/* Icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#8c6df9"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="codinStruct" />
+        <meta name="application-name" content="codinStruct" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#232136" />
+
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap"
